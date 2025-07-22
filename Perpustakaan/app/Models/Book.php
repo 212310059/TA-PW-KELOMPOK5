d72d010693740e87a886171eb7e3f0d6d8bf6b5f
InsanFajri18 <113355382+insanfajri18@users.noreply.github.com>
2025-07-23 00:38:15 +0700

    Update Book.php

--- Perpustakaan/app/Models/Book.php
+++ Perpustakaan/app/Models/Book.php
@@ -5,7 +5,6 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-// Nama class diubah menjadi singular: Book
 class Book extends Model
 {
     use HasFactory;
@@ -18,10 +17,10 @@ class Book extends Model
         return $this->belongsTo(Category::class, 'category_id');
     }
 
-    // Nama relasi diubah menjadi plural standar: borrows
+    
     public function borrows()
     {
-        // Pastikan nama model di sini juga singular: Borrow::class
+       
         return $this->hasMany(Borrow::class, 'books_id');
     }
 }
