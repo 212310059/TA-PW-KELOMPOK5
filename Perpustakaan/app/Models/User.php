0439f475a197bc617cfda002ed6f19af83167584
InsanFajri18 <113355382+insanfajri18@users.noreply.github.com>
2025-07-23 00:39:49 +0700

    Update User.php

--- Perpustakaan/app/Models/User.php
+++ Perpustakaan/app/Models/User.php
@@ -1,54 +1,28 @@
 <?php
 
 namespace App\Models;
-
-// use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Laravel\Sanctum\HasApiTokens;
-use App\Models\Borrow; // <-- DITAMBAHKAN: Import untuk relasi Borrow
+use App\Models\Borrow; 
 
 class User extends Authenticatable
 {
-    /**
-     * The "use" statement for the traits Laravel provides.
-     */
     use HasApiTokens, HasFactory, Notifiable;
-
-    /**
-     * The attributes that are mass assignable.
-     *
-     * @var array<int, string>
-     */
+    
     protected $fillable = [
         'name',
         'email',
         'password',
     ];
-
-    /**
-     * The attributes that should be hidden for serialization.
-     *
-     * @var array<int, string>
-     */
     protected $hidden = [
         'password',
         'remember_token',
     ];
-
-    /**
-     * The attributes that should be cast.
-     *
-     * @var array<string, string>
-     */
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
-
-    /**
-     * Mendefinisikan relasi "one-to-many" ke model Borrow.
-     */
     public function borrows()
     {
         return $this->hasMany(Borrow::class, 'users_id');
